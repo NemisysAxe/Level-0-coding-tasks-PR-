2fe36a3b2198d8bb2d208341d0524c1a380e2895
Xolani <xzchauke@gmail.com>
2022-05-07 15:34:07 +0200

    uploading file adjustments.

--- task_0_6.py
+++ task_0_6.py
@@ -1,28 +1,17 @@
 #task_0_6
 
-#This function returns the highest of three vaules
-#def maximum(value1, value2, value3):
- #   if (value1 > value2 and value1 > value3):
-  #      print(value1)    
-   # elif (value2 > value1 and value2 > value3):
-    #     print(value2)
-    #elif (value3 > value1 and value3 > value2) :
-     #   print(value3)
-    
-#maximum(50, 60, 5)
-
-#Bonus - this is the adjusted version of the function presented above.
-    
+#This function returns the highest of three vaules    
 def maximum(*x):
-    result = x[0] 
+    large_num = x[0]
+    
     for values in x:
-        if values > result:
-            result = values
-            return result
-        
+        if values > large_num:
+            large_num = values
+    
+    return large_num
 
-maxi_2 = maximum(1,22,3,2)
-
+#maxi_2 = maximum(1,22,3,2)
+maxi_2 = maximum(1,5,27,50)
 print(maxi_2)
 
 
--- task_0_9.py
+++ task_0_9.py
@@ -1,14 +1,24 @@
 #task_0_9
 
 #This function returns the vowels from a single string value
-def pvowels (string, vowels):
-    solution = [word1 for word1 in string if word1 in vowels]
-    print(solution)             
+def pvowels (string):
 
-string = str(" ")
-vowels = "aeiou"
+    vowels_upper = "AEIOU"
+    vowels_lower = "aeiou"
+    vowels = vowels_lower + vowels_upper
+    
+    for vowel in string:
+        if vowel in vowels:
+            if vowel > vowels_upper:  
+                if vowel in vowels_lower:
+                    print(vowel,end= ", ")
+            
+            #if vowel < vowels:
+               # print(vowels,end= ", ")
 
-pvowels('Umuzi', vowels)
+                  
+    
+pvowels('Umuzi')
 
 
 
