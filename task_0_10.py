550ae562d2c255b88f64a29db7526c4622e82556
Xolani <xzchauke@gmail.com>
2022-06-11 11:32:44 +0200

    Updated the functions.

--- task_0_10.py
+++ task_0_10.py
@@ -1,13 +1,13 @@
 
 def print_common_letters (char1, char2):
-    
-    vowels_uppercase = "AEIOU"
-    vowels_lowercase = "aeiou"
-    vowels = vowels_uppercase + vowels_lowercase         
+    #vowels_uppercase = "AEIOU"
+    #vowels_lowercase = "aeiou"
+    #vowels = vowels_uppercase + vowels_lowercase         
+    vowels = "AEIOU".lower()
     for word1 in char1 and char2:
         if word1 in vowels and char1 and char2:
             print(word1, end= ",")
-    
+
     for word1 in char1 and char2:
         if word1 in char1 and char2:
             if word1 not in vowels:
--- task_0_5.py
+++ task_0_5.py
@@ -1,18 +1,18 @@
 
-def triangle_area(a, b, c):
+def triangle_area(height, base, length):
     
-    semiparameter = (a + b + c) *0.5               #constant variable = 0.5
+    semiparameter = (height + base + length) *0.5               #constant variable = 0.5
     
-    a1 = semiparameter - a
-    b2 = semiparameter - b
-    c1 = semiparameter - c
-    y = semiparameter * (a1 * b2 * c1)
+    height1 = semiparameter - height
+    base1 = semiparameter - base
+    length1 = semiparameter - length
+    y = semiparameter * (height1 * base1 * length1)
     
-    triangle_area = y ** 0.5
+    x = y ** 0.5
     
-    x = triangle_area
+    triangle_area = x
     
-    return x
+    return triangle_area
   
 
 print(triangle_area(3, 4, 5))
