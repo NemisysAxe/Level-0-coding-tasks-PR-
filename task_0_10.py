64d14a396a53269b56f9789a35503f6af9a866a5
Xolani <xzchauke@gmail.com>
2022-06-27 10:21:47 +0200

    updated the functions

--- task_0_10.py
+++ task_0_10.py
@@ -1,6 +1,7 @@
 
 def print_common_letters (char1, char2):
-         
+    
+    phrase = "Common letters"     
     vowels = "AEIOU".lower()
     for word1 in char1 and char2:
         if word1 in vowels and char1 and char2:
@@ -11,4 +12,8 @@ def print_common_letters (char1, char2):
             if word1 not in vowels:
                 print(word1,end="'")
          
+    #print_common_letters('house','computers')
+    
+print("'Common letters: " , end ='') 
 print_common_letters('house','computers')
+
